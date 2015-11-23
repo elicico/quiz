f87864036b974baa863387c445060a612b778b9a
Elise Cicognani <e.cicognani@cantierecreativo.net>
2015-11-23 18:20:31 +0100

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,12 @@
+import 'babel-polyfill'
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+
+document.addEventListener('DOMContentLoaded', function() {
+  ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+  );
+});
