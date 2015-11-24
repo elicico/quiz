35802cbf25d5c90458de830b66fb42aba5e7fb96
Elise Cicognani <e.cicognani@cantierecreativo.net>
2015-11-24 10:45:25 +0100

    init

--- src/js/App.js
+++ src/js/App.js
@@ -1,12 +1,39 @@
 import React, { Component, PropTypes } from 'react'
+import questions from './questions.json'
+import matches from './matches.json'
+
+const ITEMS = Object.keys(questions)
 
 export default class App extends Component {
 
   render() {
     return (
       <div className="app">
-        lol
+        { ITEMS.map(this.renderQuestions.bind(this)) }
       </div>
     )
   }
+
+  renderQuestions(item) {
+    return (
+      <div>
+        <p>{ questions[ item ].question }</p>
+        <ul>
+          { questions[ item ].choices.map(this.renderChoices.bind(this, item)) }
+        </ul>
+      </div>
+    )
+  }
+
+  renderChoices(item, letter) {
+    let letters = Object.keys(questions[ item ].choices)
+    console.log(questions[ item ].choices[letter])
+    return (
+      <li>
+        <span>{ letters[letter] }</span>
+        { questions[ item ].choices[letter] }
+      </li>
+    )
+  }
+
 }
